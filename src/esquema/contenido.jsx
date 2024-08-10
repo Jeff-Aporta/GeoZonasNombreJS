f214e8bb08d66078dd324549e02799603c175bc8
Jeffrey Agudelo <jeffaporta@gmail.com>
2024-08-10 15:44:42 -0500

    GeoZonasNombreJS

---
+++ src/esquema/contenido.jsx
@@ -0,0 +1,24 @@
+const _GeoZonasNombre_ = new GenerarContenidoLibreria({
+    nombre: "Geo-zonas-nombre JS",
+    slogan: "Adapta tu diseño web de forma controlada según el ancho de la ventana",
+    img: 'src/img/logo.jpeg',
+
+    github: "https://github.com/Jeff-Aporta/CSScmds",
+
+    resumen: {
+        desc: `
+            *CSScmds* implementa un sistema avanzado de manejo y manipulación de estilos CSS dinámicos basado en comandos 
+            personalizados. Está diseñado para modificar estilos en función de condiciones específicas y permite la 
+            definición de reglas CSS que responden a ciertos parámetros, como el tamaño de la pantalla, mediante 
+            comandos de estilo encapsulados.
+        `,
+        descImg: [
+            "*Interpolación Lineal:* Ajusta suavemente los elementos de la interfaz según el ancho de la ventana.",
+            "*Condiciones Dinámicas:* Define comportamientos específicos para diferentes rangos de ancho.",
+            "*Switches Inteligentes:* Cambia entre diferentes estilos y configuraciones de manera automática.",
+            "*Fácil Integración:* Compatible con cualquier proyecto web y fácil de implementar.",
+        ]
+    },
+
+    secciones: []
+});
